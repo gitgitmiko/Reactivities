dfe9be62bb4796949180f56a61714f5ea15ecd95
Sigit Jatmiko <sigit.jatmiko@iconpln.co.id>
2024-06-24 20:41:29 +0700

    end of section 5

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
-import App from './App.tsx'
-import './index.css'
+import './app/layout/styles.css'
+import App from './app/layout/App';
 
 ReactDOM.render(
   <App />,
