762ae4789635e9b0381ba1dde3bdb4278621c337
gitgitmiko <sjatmiko@gmail.com>
2024-08-08 20:19:37 +0700

    my computer

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+111803571f63c054186a39750447df2efad47049")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b7e195c4e96760446a6cf8c42d3a0f723e9766")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
